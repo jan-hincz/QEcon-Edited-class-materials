e79e4f9d9385997ac54a15ac5196968be6c2c9bf
jan-hincz <jan.hincz@outlook.com>
2025-01-20 05:19:43 +0100

    work in progress

--- 8_dynprog/8_0_mccall_jh.jl
+++ 8_dynprog/8_0_mccall_jh.jl
@@ -1,9 +1,13 @@
-
 ## McCall Job Search model
 
 #Code based on Sargent and Stachursky (2023) - https://github.com/QuantEcon/book-dp1
-# load some packages we will need today
+
 using Distributions, Plots #Distributions package useful for statistics and calculus
+
+#TAKEAWAYS
+#line ca. 42: ...: unpacks an object contained in model - passes arguments n, w_vals, ϕ etc (everything stored in it)
+#if Beta is low, then I'm impatient, so the option value of waiting for a higher wage will be not very valuable
+#I'll accept lower wages then
 
 function create_job_search_model(;
     n=100, # wage grid size - number of possible wage offers
@@ -14,38 +18,40 @@ function create_job_search_model(;
     β=0.96, # discount factor
     c=10.0 # unemployment compensation
     )
-    w_vals = collect(LinRange(w_min, w_max, n)) #n
-    ϕ = pdf(BetaBinomial(n-1, a, b)) #n-1 vs. n
+    w_vals = collect(LinRange(w_min, w_max, n)) #n=100 possible wage values, equally spaced
+    ϕ = pdf(BetaBinomial(n-1, a, b)) #n-1 vs. n; PMF for all possible wage values
     return (; n, w_vals, ϕ, β, c)
 end
 
 my_model = create_job_search_model()
-my_model.w_vals
-plot(my_model.w_vals,my_model.ϕ) #pdf of wage offers
+my_model.w_vals #n=100 possible wage values, equally spaced
+plot(my_model.w_vals,my_model.ϕ) #interpolated PMF of wage offers
 
 #########tutorial
-my_dist = Binomial(10, 0.25)
-my_pdfs = pdf(my_dist, 0.12)
-my_cdfs = cdf(my_dist, 0)
+my_dist = Binomial(10, 0.25) #n = 10
+my_pdfs = pdf(my_dist, 0.12) #pmf at 0.12 = 0 (Binomial -> only natural numbers <= 10 have positive pmf) 
+my_cdfs = cdf(my_dist, 0) #pr(my_dist <= 0) = pmf at 0 (because 0 is minimal possible #)
 #even convolutions of functions are possible (but you gotta remember syntax)
 
 
-### APPROACH I: directly solve for h*
+### APPROACH I: directly solve for h* - slide 22 - PROBABLY NOT OPTIMAL
+#this is primitive approach I from slide 22, probably better to use NLsolve, Roots or other package to do that
+#iteratively solving to find h -> solution where h_old = h_new ()
 
 get_h_new(h_old ;n, w_vals, ϕ, β, c) = c + β * sum(ϕ[i] * max(h_old,w_vals[i]/(1-β)) for i in 1:n) #ϕ[i] = prob of a given wage
-wrap_h_new(h_old) = get_h_new(h_old; my_model...) #...: unpacks an object contained in my model - passes arguments n, w_vals, ϕ etc (everything stored in it)
+wrap_h_new(h_old) = get_h_new(h_old; my_model...) 
+#...: unpacks an object contained in model - passes arguments n, w_vals, ϕ etc (everything stored in it)
 
-h_vec = LinRange(0, 2500, 100)
+h_vec = LinRange(0, 2500, 100) #100 elements equally spaced from 0 to 2500
 plot(h_vec,wrap_h_new.(h_vec), label="h_new(h_old)",linewidth=4,xlabel = "h_old",ylabel = "h_new");
 plot!(h_vec,h_vec, label="45 degree line", linestyle=:dash,linewidth=2)
 
-#iteratively solving to find h -> solution where h_old = h_new ()
-#this is primitive approach I from slide 22, probably better to use NLsolve, Roots or other package to do that
 
 function get_h_1(model;tol=1e-8,maxiter=1000,h_init=model.c/(1-model.β)) # do it by hand
     
     (; n, w_vals, ϕ, β, c) = model # unpack the model parameters
 
+    #initial values
     h_old = Float64(h_init)
     h_new = h_old
     h_history = [h_old]
@@ -53,60 +59,60 @@ function get_h_1(model;tol=1e-8,maxiter=1000,h_init=model.c/(1-model.β)) # do i
     iter = 1
 
     while error > tol && iter < maxiter
-        h_new = get_h_new(h_old ;n, w_vals, ϕ, β, c) 
-        error = abs(h_new - h_old) # think about this error criterion... why is it not great?
-        h_old = h_new
-        push!(h_history,h_old)
+        h_new = get_h_new(h_old ;n, w_vals, ϕ, β, c) #function defined at ca. 41 line
+        error = abs(h_new - h_old) # not great error criterion, would be better in relative terms vs. h_old
+        h_old = h_new #h_new becomes h_old during next iteration
+        push!(h_history,h_old) #consecutive h_old values put into h_history vector
         iter += 1
     end
 
     return h_new, iter, error, h_history
 end
 
-h, iter, error, h_history = get_h_1(my_model)
+h, iter, error, h_history = get_h_1(my_model) #look at last line of function code: last h_new becomes h - the solution value 
+#h_history has 24 h (each 1 from a given iteration)
 
-function get_v_from_h(model,h) 
+function get_v_from_h(model,h) #slides 45-46
     
     (; n, w_vals, ϕ, β, c) = model # unpack the model parameters
     σ = w_vals ./ (1-β) .>= h # this is a vector of booleans; σ = 1 when it's optimal to accept, σ = 0 when it's optimal to reject
-    v = σ .* w_vals ./ (1-β) + (1 .- σ) .* h # this is a vector of floats
+    v = σ .* w_vals ./ (1-β) + (1 .- σ) .* h # this is a vector of floats - see slide 46
     return v, σ 
 end
 
-v, σ = get_v_from_h(my_model,h)
+v, σ = get_v_from_h(my_model,h) #vector v - each entry for a given w: for lowest wages v(w) = h = 1070.44; for w >= reservation wage v(w) = w/(1-β) 
 
 plot_v = plot(my_model.w_vals,v, label="v(w)",linewidth=4,xlabel = "w",ylabel = "v")
 plot_σ = plot(my_model.w_vals,σ, label="policy: 1 = accept wage",xlabel = "w", linestyle=:dash,linewidth=2)
 
 plot(plot_v,plot_σ,layout=(1,2),legend=:topleft) #optimal to reject under #42.82 = reservation wage - see below
 
-reservation_wage = my_model.w_vals[σ][1] #42.82
+reservation_wage = my_model.w_vals[σ][1] #[σ] works as filter to only take wages for which σ = 1; 
+#[σ][1]: 1st element (because wages set in increasing order) when it is optimal to accept -> reservation wage 42.82
 
 # study how reservation wage depends on unemployment compensation
-c_vec = LinRange(0, 60, 100)
+c_vec = LinRange(0, 60, 100) #100 elements from 0 to 60
 reservation_wage_vec = []
 for c in c_vec
-    my_model = create_job_search_model(;c=c)
-    h, iter, error, h_history = get_h_1(my_model)
-    v, σ = get_v_from_h(my_model,h)
-    push!(reservation_wage_vec,my_model.w_vals[σ][1])
+    my_model = create_job_search_model(;c=c) #line 12
+    h, iter, error, h_history = get_h_1(my_model) #line 50
+    v, σ = get_v_from_h(my_model,h) #line 75
+    push!(reservation_wage_vec,my_model.w_vals[σ][1]) #reservation wage (line 90) for each c from c_vec
 end
 
 plot_reservation_wage = plot(c_vec,reservation_wage_vec, label=false,linewidth=4,xlabel = "unemp. compensation",ylabel = "reservation wage")
-#if Beta is low, then i'm impatient so the option value of waiting for a higher wage will be not very valuable to makes
-#I'll accept lower wages then
 
 
-### APPROACH II: dynamic programming #THIS APPROACH MIGHT NOT BE THE FASTEST, BUT IT'S GUARANTEED TO WORK (WITHIN REASON)
+### APPROACH II: dynamic programming: THIS APPROACH MIGHT NOT BE THE FASTEST, BUT IT'S GUARANTEED TO WORK (WITHIN REASON)
 
 function T(v,model) # Bellman operator: T (slide 41)
     (; n, w_vals, ϕ, β, c) = model # unpack the model parameters
-    return [max(w/(1-β) , c+β * v'ϕ) for w in w_vals] #logical statement -> returns 0 or 1
+    return [max(w/(1-β) , c+β * v'ϕ) for w in w_vals]
 end
 
 function get_policy(v,model) # this will be used after finding the fixed point of T
     (; n, w_vals, ϕ, β, c) = model # unpack the model parameters
-    return σ = [w/(1-β) >= c+β * v'ϕ for w in w_vals]
+    return σ = [w/(1-β) >= c+β * v'ϕ for w in w_vals] #slide 46: logical statement -> returns 0 or 1
 end
 
 function vfi(model;maxiter=1000,tol=1e-8) # value function iteration
@@ -115,18 +121,18 @@ function vfi(model;maxiter=1000,tol=1e-8) # value function iteration
     v = v_init
     v_history = [v_init]
     while error > tol && iter < maxiter
-        v_new = T(v,model)
+        v_new = T(v,model) #defined at line ca. 109
         error = maximum(abs.(v_new - v)) #difference between 2 consecutive v
         push!(v_history,v_new)
-        v = v_new
+        v = v_new #old v_new becomes v for the next iteration
         iter += 1
     end
-    σ = get_policy(v, model)
+    σ = get_policy(v, model) #defined at line ca. 114
 
     return v, σ, iter, error, v_history #value function, policy function [...]
 end
 
-my_model = create_job_search_model()
+my_model = create_job_search_model() #line ca. 13
 v, σ, iter, error, v_history = vfi(my_model)
 plot_v = plot(my_model.w_vals,v, label="v(w)",linewidth=4,xlabel = "w",ylabel = "v")
 plot_σ = plot(my_model.w_vals,σ, label="policy: 1 = accept wage",xlabel = "w", linestyle=:dash,linewidth=2)
---
+++ 8_dynprog/8_1_d_ngm_jh.jl
@@ -0,0 +1,133 @@
+## Neoclassical growth model - slides 19 - 24
+using  Plots, Parameters
+
+#TAKEAWAYS
+#findfirst() - line ca. 100
+
+@with_kw struct NGMProblem #@with_kw: macro of Parameters package
+
+    β = 0.95 # discount factor
+    α = 0.3 # production function parameter - slide 20
+    δ = 0.1 # depreciation rate
+    γ = 2.0 # intertemporal elasticity of substitution (inverse) - slide 20 (as gamma in slides)
+
+    f = x -> x^α # production function
+    u = γ == 1 ? c -> log(c) : c -> c ^ (1-γ) / (1-γ)   # CRRA utility function
+    k_star = ((β^(-1) - 1 + δ) / α) ^(1/(α-1)) # steady state capital (slide 23)
+
+    k_min = 0.75 * k_star # minimum capital
+    k_max = 1.25 * k_star # maximum capital
+    
+    n = 100 # number of grid points
+    k_grid = range(k_min,stop=k_max,length=n) #n-element grid for capital from k_min to k_max
+    
+end
+
+
+### NGM PROBLEM
+
+function T(v,model) # Bellman operator
+    @unpack n, k_grid, β, α, δ, f, u = model #Parameters package -> unpack -> it will take
+    #arguments named this way from model of choice
+
+    #initialise the function
+    v_new = zeros(n) #v(k) as a vector, each entry will be for a given k 
+    reward = zeros(n,n) #matrix n by n of lifetime utility for all possible k and k'
+    #max lifetime utility wrt k' for a given k = v(k)
+    σ = zeros(n)
+
+        for (k_index,k) in enumerate(k_grid) # loop over capital today
+            for (k_next_index, k_next) in enumerate(k_grid) # loop over capital tomorrow
+
+                c = k^α - k_next + (1-δ)*k # consumption (slide 20)
+                if c > 0
+                    reward[k_index,k_next_index] = u(c) + β * v[k_next_index] #v(k) entry; (slide 21)
+                else
+                    reward[k_index,k_next_index] = -Inf #penalty instead of constrained optimisation with c>0
+                end
+
+            end 
+
+            v_new[k_index], k_next_index_opt = findmax(reward[k_index,:]) 
+            # k_index -> for each k, find the maximum reward = v(k) and the optimal next level of capital k' (with k_next_index_opt)
+            σ[k_index] = k_grid[k_next_index_opt] # store the optimal policy σ(k) = k'
+        end
+        
+    return v_new, σ
+end
+
+
+function vfi(model;maxiter=1000,tol=1e-8) # value function iteration
+    @unpack n, k_grid, β, α, δ, f, u = model
+    v_init = zeros(n); err = tol + 1.0; iter = 1 #initial guess
+    v = v_init
+    v_history = [v_init]
+    σ = zeros(n)
+    while err > tol && iter < maxiter
+        v_new, σ = T(v,model)
+        err = maximum(abs.(v_new - v)) 
+        push!(v_history,v_new)
+        v = v_new
+        iter += 1
+    end
+
+
+    return v, σ, iter, err, v_history
+end
+
+
+my_ngm = NGMProblem(n=200) #defined at ca. line 4
+v, σ, iter, err, v_history = vfi(my_ngm)
+v #v(k) for each of 200 possible k
+
+plot_v = plot(my_ngm.k_grid,v, label="v(k)",linewidth=4,xlabel = "k",ylabel = "v");
+plot_σ = plot(my_ngm.k_grid,σ, label="policy: k'(k)", linewidth=4,xlabel = "k",);
+
+# add the 45 degree line
+plot!(my_ngm.k_grid,my_ngm.k_grid, label="45 degree line",linewidth=2,linestyle=:dash);
+
+# add the steady state
+vline!([my_ngm.k_star], label="steady state",linewidth=2,linestyle=:dash);
+plot(plot_v,plot_σ,layout=(1,2),legend=:topleft)
+
+# obtain a sample path for the capital stock
+
+Time = 100
+k_path = zeros(Time)
+k_path[1] = my_ngm.k_grid[1] # start at the lowest level of capital 1.969 = 0.75 * k_star
+
+for i in 2:Time
+    k_path[i] = σ[findfirst(x->x==k_path[i-1],my_ngm.k_grid)] #for an i-1th k it finds ith (next) k based on σ(k) = k' function 
+    #in Julia you cannot call σ(k) = σ(k_path[i-1]),
+    #you have to do σ[index of k] and that's what findfirst() does here
+end
+
+plot_k_path = plot(1:Time,k_path, label="k(t)",linewidth=4,xlabel = "t",ylabel = "k")
+
+σ γ
+# compare the speed of convergence for two different elasticities of substitution
+
+my_ngm_low_γ = NGMProblem(γ=0.5,n=300)
+v_low_γ, γ_low_γ, iter_low_γ, err_low_γ, v_history_low_γ = vfi(my_ngm_low_γ)
+
+my_ngm_high_γ = NGMProblem(γ=5.0,n=300)
+v_high_γ, γ_high_γ, iter_high_γ, err_high_γ, v_history_high_γ = vfi(my_ngm_high_γ)
+
+Time = 100 
+k_path_low_γ = zeros(Time)
+k_path_high_γ = zeros(Time)
+k_path_low_γ[1] = my_ngm_low_γ.k_grid[1] # start at the lowest level of capital
+k_path_high_γ[1] = my_ngm_high_γ.k_grid[1] # start at the lowest level of capital
+
+for i in 2:Time
+    k_path_low_γ[i] = γ_low_γ[findfirst(x->x==k_path_low_γ[i-1],my_ngm_low_γ.k_grid)]
+    k_path_high_γ[i] = γ_high_γ[findfirst(x->x==k_path_high_γ[i-1],my_ngm_high_γ.k_grid)]
+end
+
+
+plot_k_convergence = plot(1:Time,k_path_low_γ, label="γ = 0.5",linewidth=4,xlabel = "t",ylabel = "k",legend=:topleft);
+plot!(1:Time,k_path_high_γ, label="γ = 5.0",linewidth=4,xlabel = "t",ylabel = "k",legend=:topleft)
+
+# what is wrong here? 
+plot(γ_high_γ - my_ngm_high_γ.k_grid)
+plot(γ_low_γ - my_ngm_low_γ.k_grid)
