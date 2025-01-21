bfad8cd3e2e82920ed70071c8e9f14d85c7c49f6
jan-hincz <jan.hincz@outlook.com>
2025-01-21 01:40:19 +0100

    PS 3.3 nearly finished

--- 8_dynprog/ps3_3_jh.jl
+++ 8_dynprog/ps3_3_jh.jl
@@ -1,23 +1,20 @@
-## Neoclassical growth model - slides 19 - 24
+## Neoclassical growth model
 using  Plots, Parameters, PrettyTables
 
-#TAKEAWAYS
-#findfirst() - line ca. 100
-#at the end - what is wrong? - ask Piotr and Marcin!!! (line ca. 133)
 
 @with_kw struct NGMProblem2 #@with_kw: macro of Parameters package
 
     β = 0.95 # discount factor
-    α = 0.3 # production function parameter - slide 20
+    α = 0.3 # production function parameter
     δ = 0.05 # depreciation rate
-    γ = 2.0 # intertemporal elasticity of substitution (inverse) - slide 20
+    γ = 2.0 # intertemporal elasticity of substitution (inverse)
 
     f = x -> x^α # production function
     u = γ == 1 ? c -> log(c) : c -> c ^ (1-γ) / (1-γ)   # CRRA utility function
-    k_star = ((β^(-1) - 1 + δ) / α) ^(1/(α-1)) # steady state capital (slide 23)
+    k_star = ((β^(-1) - 1 + δ) / α) ^(1/(α-1)) # steady state capital
 
     k_min = 0.5 * k_star # minimum capital
-    k_max = 1.25*k_star # maximum capital - MAYBE CHANGE TO 1 * ########################
+    k_max = 1.25*k_star # maximum capital
     
     n = 100 # number of grid points
     k_grid = range(k_min,stop=k_max,length=n) #n-element grid for capital from k_min to k_max
@@ -28,21 +25,19 @@ end
 ### NGM PROBLEM2
 
 function T(v,model) # Bellman operator
-    @unpack n, k_grid, β, α, δ, f, u = model #Parameters package -> unpack -> it will take
-    #arguments named this way from model of choice
+    @unpack n, k_grid, β, α, δ, f, u = model
 
     #initialise the function
-    v_new = zeros(n) #v(k) as a vector, each entry will be for a given k 
-    reward = zeros(n,n) #matrix n by n of lifetime utility for all possible k and k'
-    #max lifetime utility wrt k' for a given k = v(k)
+    v_new = zeros(n)
+    reward = zeros(n,n) 
     σ = zeros(n)
 
         for (k_index,k) in enumerate(k_grid) # loop over capital today
             for (k_next_index, k_next) in enumerate(k_grid) # loop over capital tomorrow
 
-                c = k^α - k_next + (1-δ)*k # consumption (slide 20)
+                c = k^α - k_next + (1-δ)*k # consumption
                 if c > 0
-                    reward[k_index,k_next_index] = u(c) + β * v[k_next_index] #v(k) entry; (slide 21)
+                    reward[k_index,k_next_index] = u(c) + β * v[k_next_index] #v(k) entry
                 else
                     reward[k_index,k_next_index] = -Inf #penalty instead of constrained optimisation with c>0
                 end
@@ -50,8 +45,7 @@ function T(v,model) # Bellman operator
             end 
 
             v_new[k_index], k_next_index_opt = findmax(reward[k_index,:]) 
-            # k_index -> for each k, find the maximum reward = v(k) and the optimal next level of capital k' (with k_next_index_opt)
-            σ[k_index] = k_grid[k_next_index_opt] # store the optimal policy σ(k) = k'
+            σ[k_index] = k_grid[k_next_index_opt]
         end
         
     return v_new, σ
@@ -77,83 +71,196 @@ function vfi(model;maxiter=1000,tol=1e-8) # value function iteration
 end
 
 
-my_ngm = NGMProblem2(n=200) #defined at ca. line 4
-v, σ, iter, err, v_history = vfi(my_ngm)
-v #v(k) for each of 200 possible k
+my_ngm_low_γ = NGMProblem2(n=300,γ=0.5)
+v_low_γ, σ_low_γ, iter_low_γ, err_low_γ, v_history_low_γ = vfi(my_ngm_low_γ)
 
-plot_v = plot(my_ngm.k_grid,v, label="v(k)",linewidth=4,xlabel = "k",ylabel = "v");
-plot_σ = plot(my_ngm.k_grid,σ, label="policy: k'(k)", linewidth=4,xlabel = "k",);
+my_ngm_mid_γ = NGMProblem2(n=300,γ=1)
+v_mid_γ, σ_mid_γ, iter_mid_γ, err_mid_γ, v_history_mid_γ = vfi(my_ngm_mid_γ)
+
+my_ngm_high_γ = NGMProblem2(n=300,γ=2)
+v_high_γ, σ_high_γ, iter_high_γ, err_high_γ, v_history_high_γ = vfi(my_ngm_high_γ)
+
+#PLOTS
+
+plot_v_low_γ = plot(my_ngm_low_γ.k_grid,v_low_γ, label="v(k)",linewidth=4,xlabel = "k",ylabel = "v");
+plot_σ_low_γ = plot(my_ngm_low_γ.k_grid,σ_low_γ, label="policy: k'(k)", linewidth=4,xlabel = "k",);
 
 # add the 45 degree line
-plot!(my_ngm.k_grid,my_ngm.k_grid, label="45 degree line",linewidth=2,linestyle=:dash);
+plot!(my_ngm_low_γ.k_grid,my_ngm_low_γ.k_grid, label="45 degree line",linewidth=2,linestyle=:dash);
 
 # add the steady state
-vline!([my_ngm.k_star], label="steady state",linewidth=2,linestyle=:dash);
-plot(plot_v,plot_σ,layout=(1,2),legend=:topleft)
+vline!([my_ngm_low_γ.k_star], label="steady state",linewidth=2,linestyle=:dash);
+plot(plot_v_low_γ,plot_σ_low_γ,layout=(1,2),legend=:topleft)
+
+
+plot_v_mid_γ = plot(my_ngm_mid_γ.k_grid,v_mid_γ, label="v(k)",linewidth=4,xlabel = "k",ylabel = "v");
+plot_σ_mid_γ = plot(my_ngm_mid_γ.k_grid,σ_mid_γ, label="policy: k'(k)", linewidth=4,xlabel = "k",);
+
+# add the 45 degree line
+plot!(my_ngm_mid_γ.k_grid,my_ngm_mid_γ.k_grid, label="45 degree line",linewidth=2,linestyle=:dash);
+
+# add the steady state
+vline!([my_ngm_mid_γ.k_star], label="steady state",linewidth=2,linestyle=:dash);
+plot(plot_v_mid_γ,plot_σ_mid_γ,layout=(1,2),legend=:topleft)
+
+
+plot_v_high_γ = plot(my_ngm_high_γ.k_grid,v_high_γ, label="v(k)",linewidth=4,xlabel = "k",ylabel = "v");
+plot_σ_high_γ = plot(my_ngm_high_γ.k_grid,σ_high_γ, label="policy: k'(k)", linewidth=4,xlabel = "k",);
+
+# add the 45 degree line
+plot!(my_ngm_high_γ.k_grid,my_ngm_high_γ.k_grid, label="45 degree line",linewidth=2,linestyle=:dash);
+
+# add the steady state
+vline!([my_ngm_high_γ.k_star], label="steady state",linewidth=2,linestyle=:dash);
+plot(plot_v_high_γ,plot_σ_high_γ,layout=(1,2),legend=:topleft)
+
+#PLOT FOR POLICY FOR ALL GAMMAS
+plot(my_ngm_low_γ.k_grid,σ_low_γ, label="γ = 0.5", linewidth=4,xlabel = "k", ylabel = "k'")
+plot!(my_ngm_mid_γ.k_grid,σ_mid_γ, label="γ = 1", linewidth=4,xlabel = "k", ylabel = "k'")
+plot!(my_ngm_high_γ.k_grid,σ_high_γ, label="γ = 2", linewidth=4,xlabel = "k", ylabel = "k'")
+vline!([my_ngm_high_γ.k_star], label="steady state",linewidth=2,linestyle=:dash)
 
 # obtain a sample path for the capital stock
 
-Time = 100
-k_path = zeros(Time)
-k_path[1] = my_ngm.k_grid[1] # start at the lowest level of capital 1.969 = 0.75 * k_star
-
-for i in 2:Time
-    k_path[i] = σ[findfirst(x->x==k_path[i-1],my_ngm.k_grid)] #for an i-1th k it finds ith (next) k based on σ(k) = k' function 
-    #in Julia you cannot call σ(k) = σ(k_path[i-1]),
-    #you have to do σ[index of k] and that's what findfirst() does here
-end
-
-plot_k_path = plot(1:Time,k_path, label="k(t)",linewidth=4,xlabel = "t",ylabel = "k")
-
-# compare the speed of convergence for two different elasticities of substitution
-
-my_ngm_low_γ = NGMProblem2(γ=0.5,n=300) #defined at line ca. 7
-v_low_γ, σ_low_γ, iter_low_γ, err_low_γ, v_history_low_γ = vfi(my_ngm_low_γ) #defined at line ca. 60
-
-my_ngm_high_γ = NGMProblem2(γ=2.0,n=300)
-v_high_γ, σ_high_γ, iter_high_γ, err_high_γ, v_history_high_γ = vfi(my_ngm_high_γ)
-
 Time = 300
 k_path_low_γ = zeros(Time)
+k_path_mid_γ = zeros(Time)
 k_path_high_γ = zeros(Time)
 k_path_low_γ[1] = my_ngm_low_γ.k_grid[1] # start at the lowest level of capital
+k_path_mid_γ[1] = my_ngm_mid_γ.k_grid[1] # start at the lowest level of capital
 k_path_high_γ[1] = my_ngm_high_γ.k_grid[1] # start at the lowest level of capital
 
 for i in 2:Time
     k_path_low_γ[i] = σ_low_γ[findfirst(x->x==k_path_low_γ[i-1],my_ngm_low_γ.k_grid)]
+    k_path_mid_γ[i] = σ_mid_γ[findfirst(x->x==k_path_mid_γ[i-1],my_ngm_mid_γ.k_grid)]
     k_path_high_γ[i] = σ_high_γ[findfirst(x->x==k_path_high_γ[i-1],my_ngm_high_γ.k_grid)]
 end
 
-#####REPAIR###############################################
 
-a = findfirst(x->k_path_low_γ[end] -x<0.5*(k_path_low_γ[end]-k_path_low_γ[1]),my_ngm_low_γ.k_grid)
-b = findfirst(x->k_path_high_γ[end] -x<0.5*(k_path_high_γ[end]-k_path_high_γ[1]),my_ngm_high_γ.k_grid)
-periods = [a,b]
-γ_vec = [0.5,2.0]
-data = hcat(periods, γ_vec)
+#Plots of convergence
+plot(1:Time,k_path_low_γ, label="k(t); γ = 0.5 ",linewidth=4,xlabel = "t",ylabel = "k")
+plot!(1:Time,k_path_mid_γ, label="k(t); γ = 1",linewidth=4,xlabel = "t",ylabel = "k")
+plot!(1:Time,k_path_high_γ, label="k(t); γ = 2",linewidth=4,xlabel = "t",ylabel = "k")
 
-header = (["time of half-convergence","γ"],["periods",""]) #[columns], [subtitles]
+
+period_low_γ = findfirst(x -> my_ngm_low_γ.k_star - x < 0.5 * (my_ngm_low_γ.k_star - my_ngm_low_γ.k_min), k_path_low_γ) #6
+period_mid_γ = findfirst(x -> my_ngm_mid_γ.k_star - x < 0.5 * (my_ngm_mid_γ.k_star - my_ngm_mid_γ.k_min), k_path_mid_γ) #8
+period_high_γ = findfirst(x -> my_ngm_high_γ.k_star - x < 0.5 * (my_ngm_high_γ.k_star - my_ngm_high_γ.k_min), k_path_high_γ) #11
+
+
+periods = [period_low_γ,period_mid_γ,period_high_γ]
+γ_vec = [0.5,1.0,2.0]
+
+data = hcat(γ_vec,periods)
+    
+header = (["γ","time of half-convergence"],["","periods"]) #[columns], [subtitles]
 
 pretty_table(data;
 header=header,
 header_crayon=crayon"yellow bold" ,
-formatters = ft_printf("%5.2f",2), #2 decimals shown
+formatters = ft_printf("%5.2f",0),
 display_size =  (-1,-1)) #to get the entire table
 
 
-#REPAIR################################
+#3.1. FUNCTION WITH TABLE WITH HALF CONVERGENCE
+
+function half_convergence(n::Int64,γ_vec::Vector)
+    periods = Int[]
+    for γ in γ_vec
+        model = NGMProblem2(γ=γ)
+        _, σ, _, _, _ = vfi(model)
+        k_path = zeros(n)
+        k_path[1] = model.k_grid[1]
+        for i in 2:n
+            k_path[i] = σ[findfirst(x -> x == k_path[i-1], model.k_grid)]
+        end
+        period = findfirst(x -> model.k_star - x < 0.5 * (model.k_star - model.k_grid[1]), k_path)
+        push!(periods, period)
+    end
+    
+    data = hcat(γ_vec,periods)
+    
+    header = (["γ","time of half-convergence"],["","periods"]) #[columns], [subtitles]
+
+    table = pretty_table(data;
+    header=header,
+    header_crayon=crayon"yellow bold" ,
+    formatters = ft_printf("%5.2f",0),
+    display_size =  (-1,-1)) #to get the entire table
+    return table
+end
+
+half_convergence(300,[0.5,1.0,2.0])
 
 
-plot_k_convergence = plot(1:Time,k_path_low_γ, label="γ = 0.5",linewidth=4,xlabel = "t",ylabel = "k",legend=:topleft);
-plot!(1:Time,k_path_high_γ, label="γ = 5.0",linewidth=4,xlabel = "t",ylabel = "k",legend=:topleft)
+#Plots of convergence
+plot_capital = plot(1:Time,k_path_low_γ, label="k(t); γ = 0.5 ",linewidth=4,xlabel = "t",ylabel = "k")
+plot!(1:Time,k_path_mid_γ, label="k(t); γ = 1",linewidth=4,xlabel = "t",ylabel = "k")
+plot!(1:Time,k_path_high_γ, label="k(t); γ = 2",linewidth=4,xlabel = "t",ylabel = "k")
+
+
+plot_output = plot(1:Time,k_path_low_γ.^my_ngm_low_γ.α, label="y(t); γ = 0.5 ",linewidth=4,xlabel = "t",ylabel = "y")
+plot!(1:Time,k_path_mid_γ.^my_ngm_mid_γ.α, label="y(t); γ = 1 ",linewidth=4,xlabel = "t",ylabel = "y")
+plot!(1:Time,k_path_high_γ.^my_ngm_high_γ.α, label="y(t); γ = 2 ",linewidth=4,xlabel = "t",ylabel = "y")
+
+
+function i_ratio(k_path::Vector,model)
+    i_ratio = zeros(length(k_path) - 1)
+    for i in 1:(length(k_path) - 1)
+    i_ratio[i] = (k_path[i+1] - (1-model.δ)*k_path[i])/(k_path[i]^model.α)
+    end
+    return i_ratio
+end
+
+i_ratio_low_γ =  i_ratio(k_path_low_γ,my_ngm_low_γ)
+i_ratio_mid_γ =  i_ratio(k_path_mid_γ,my_ngm_mid_γ)
+i_ratio_high_γ =  i_ratio(k_path_high_γ,my_ngm_high_γ)
+
+
+plot_i_ratio = plot(1:(Time-1),i_ratio_low_γ, label="i_ratio(t); γ = 0.5 ",linewidth=4,xlabel = "t",ylabel = "i ratio")
+plot!(1:(Time-1),i_ratio_mid_γ, label="i_ratio(t); γ = 1 ",linewidth=4,xlabel = "t",ylabel = "i ratio")
+plot!(1:(Time-1),i_ratio_high_γ, label="i_ratio(t); γ = 2 ",linewidth=4,xlabel = "t",ylabel = "i ratio")
+
+plot_c_ratio = plot(1:(Time-1),1 .- i_ratio_low_γ, label="c_ratio(t); γ = 0.5 ",linewidth=4,xlabel = "t",ylabel = "c ratio")
+plot!(1:(Time-1),1 .-i_ratio_mid_γ, label="c_ratio(t); γ = 1 ",linewidth=4,xlabel = "t",ylabel = "c ratio")
+plot!(1:(Time-1),1 .-i_ratio_high_γ, label="c_ratio(t); γ = 2 ",linewidth=4,xlabel = "t",ylabel = "c ratio")
+
+plot(plot_capital,plot_output,plot_i_ratio,plot_c_ratio,layout=(2,2),legend=:topright)
+
+#3.2. FUNCTION WITH CONVERGENCE PLOTS
+
+
+function convergence_plots(Time::Int64,γ_vec::Vector)
+    plot_capital = plot()
+    plot_output = plot()
+    plot_i_ratio = plot()
+    plot_c_ratio = plot()
+    for γ in γ_vec
+        model = NGMProblem2(γ=γ)
+        _, σ, _, _, _ = vfi(model)
+        k_path = zeros(Time)
+        k_path[1] = model.k_grid[1]
+
+        for i in 2:Time
+            k_path[i] = σ[findfirst(x -> x == k_path[i-1], model.k_grid)]
+        end
+        i_ratio = zeros(length(k_path) - 1)
+
+        for i in 1:(length(k_path) - 1)
+        i_ratio[i] = (k_path[i+1] - (1-model.δ)*k_path[i])/(k_path[i]^model.α)
+        end
+        
+        plot_capital = plot!(plot_capital,1:Time,k_path, label="k(t); γ = $γ",linewidth=4,xlabel = "t",ylabel = "k")
+        plot_output = plot!(plot_output,1:Time,k_path.^model.α, label="y(t); γ = $γ",linewidth=4,xlabel = "t",ylabel = "y")
+        plot_i_ratio = plot!(plot_i_ratio,1:(Time-1),i_ratio, label="i_ratio(t); γ = $γ",linewidth=4,xlabel = "t",ylabel = "i ratio")
+        plot_c_ratio = plot!(plot_c_ratio,1:(Time-1),1 .- i_ratio, label="c_ratio(t); γ = $γ",linewidth=4,xlabel = "t",ylabel = "c ratio")
+    end
+    
+    return plot(plot_capital,plot_output,plot_i_ratio,plot_c_ratio, layout=(2,2),legend=:topright)
+end
+
+convergence_plots(200,[0.5,1.0,2.0])
+
+
 #quicker convergence for lower γ - less concave function - lower consumption smoothing motive
 #-> will get quicker to steady state of c and k by in the meantime saving more and sacrificing c
-
-# what is wrong here? 
-plot(σ_high_γ - my_ngm_high_γ.k_grid) #IMO: k' - k, it should be 0 for k_star = 2.625 and it's 0 for k +-= 130
-plot(σ_low_γ - my_ngm_low_γ.k_grid)
-
-#Chat GPT suggests
-#The discrepancy likely arises from grid resolution or numerical precision issues. 
-#Adjusting the grid size or refining the numerical methods can help achieve a more accurate solution,
-#aligning the policy function's steady state with the theoretical steady state capital k*
